88797e378bfb0db4ac9f0503faf430cd8e1a1681
xirothedev <lethanhtrung.trungle@gmail.com>
2025-07-09 13:07:20 +0700

    Update CI workflow to use ESLint and Prettier for linting and formatting checks; remove unnecessary ESLint disable comments from chart component

--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/restrict-template-expressions */
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unnecessary-condition */
 "use client";
